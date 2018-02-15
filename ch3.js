1914752e13b77ca96a87135e8d6221b626f9f208
Jenny Graft <jenny.graft@gmail.com>
2018-02-15 15:00:07 -0500

    added = signs

--- ch3.js
+++ ch3.js
@@ -25,9 +25,9 @@ console.log(min(0, -10));
  ********************/
 
 function isEven(n) {
-    if (n == 0)
+    if (n === 0)
         return true;
-    else if (n == 1)
+    else if (n === 1)
         return false;
     else if (n < 0)
         return isEven(-n);
@@ -46,9 +46,9 @@ console.log(isEven(-1));
  ************************/
 
 function countChar(string, ch) {
-    var counted = 0;
-    for (var i = 0; i < string.length; i++)
-        if (string.charAt(i) == ch)
+    let counted = 0;
+    for (let i = 0; i < string.length; i++)
+        if (string.charAt(i) === ch)
             counted += 1;
     return counted;
 }
